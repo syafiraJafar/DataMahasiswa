378a36bb383a843a48ee18a65ebf6fcac22ccffc
syafira <Syafirajafar30@gmail.com>
2020-04-13 19:13:05 +0700

    Pengerjaan sudah selesai tetapi belum ada method run

---
+++ app/src/main/java/com/example/datamahasiswa/DisplayMhs.java
@@ -0,0 +1,72 @@
+package com.example.datamahasiswa;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayMhs extends AppCompatActivity {
+    int from_Where_I_Am_Coming = 0;
+    private com.example.datamahasiswa.DBHelper mydb ;
+    TextView nomhs ;
+    TextView phone;
+    TextView nama;
+    int id_To_Update = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_mhs);
+        nomhs = (TextView) findViewById(R.id.editTextNim);
+        nama = (TextView) findViewById(R.id.editTextName);
+        phone = (TextView) findViewById(R.id.editTextPhone);
+        mydb = new com.example.datamahasiswa.DBHelper(this);
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null) {
+            int Value = extras.getInt("id");
+            if(Value>0){
+                //means this is the view part not the add contact part.
+                Cursor rs = mydb.getData(Value);
+                id_To_Update = Value;
+                rs.moveToFirst();
+                String no =
+                        rs.getString(rs.getColumnIndex(com.example.datamahasiswa.DBHelper.MHS_COLUMN_NIM));
+                String nam =
+                        rs.getString(rs.getColumnIndex(com.example.datamahasiswa.DBHelper.MHS_COLUMN_NAMA));
+                String phon =
+                        rs.getString(rs.getColumnIndex(com.example.datamahasiswa.DBHelper.MHS_COLUMN_PHONE));
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                Button b = (Button)findViewById(R.id.button1);
+                b.setVisibility(View.INVISIBLE);
+                nomhs.setText((CharSequence)no);
+                nomhs.setFocusable(false);
+                nomhs.setClickable(false);
+                nama.setText((CharSequence)nam);
+                nama.setFocusable(false);
+                nama.setClickable(false);
+                phone.setText((CharSequence)phon);
+                phone.setFocusable(false);
+                phone.setClickable(false);
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+                Bundle extras = getIntent().getExtras();
+        if(extras !=null) {
+            int Value = extras.getInt("id");
+            if(Value>0){
+                getMenuInflater().inflate(R.menu.menu_display,menu);
+            } else{
+                getMenuInflater().inflate(R.menu.menu_main,menu);
+            }
+        }
+        return true;
+    }
+}
